132a783cd8d559ddc617d2d0257a59f587f2993c
KyLS2005 <kylian.le-scanff@eleves.ecmorlaix.fr>
2022-11-24 15:47:55 +0000

    24-11-2022--18-47

--- TPE/ds2_le_scanff_kylian_copy.py
+++ TPE/ds2_le_scanff_kylian_copy.py
@@ -16,20 +16,19 @@ def depiler(pile):
     assert not est_vide(pile), "Pile vide"
     return pile.pop()
 
+# Première implémentation pour les piles
 def sommet(pile):
-    sommet = creer_pile()
     ''' Renvoie la valeur au sommet de la pile mais sans la supprimer de la pile '''
-    if est_vide(pile) == True :
+    if est_vide(pile):
         raise IndexError('pile vide')
     else:
-        sommet = depiler(pile)
-        empiler(pile,sommet)
-        return sommet
+        valeur = depiler(pile)
+        empiler(pile, valeur)
+    return valeur
 
 def mettre_disques(pile, n):
-    '''met des disques de taille n à 1 sur la pile'''
-    for i in list(range(n,0,-1)):
-        empiler(pile,i)
+    for i in range(n, 0, -1):
+        empiler(pile, i)
 
 def creation_tours(n):
     ''' renvoie une liste de 3 piles,
@@ -40,5 +39,4 @@ def creation_tours(n):
     p2 = creer_pile()
     mettre_disques(p0, n)
     return [p0, p1, p2]
-
     
